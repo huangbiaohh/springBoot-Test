2a0a63e2ccef4491cc31ec1cf1cd6d43a7dd7d21
huangbiao <huangbiao@asiainfo.com>
2019-12-31 16:44:11 +0800

    控制层

--- src/main/java/cn/com/test.java
+++ src/main/java/cn/com/test.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class test {
 
+    
     @RequestMapping("/hello")
     public String t(){
         return "Hello Wold!";
