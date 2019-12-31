a68f1ef6f3bef321843113774b4feae0ddb370ac
huangbiao <huangbiao@asiainfo.com>
2019-12-31 10:09:29 +0800

    web测试

---
+++ src/main/java/cn/com/test.java
@@ -0,0 +1,13 @@
+package cn.com;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+
+    @RequestMapping("/hello")
+    public String t(){
+        return "Hello Wold!";
+    }
+}
